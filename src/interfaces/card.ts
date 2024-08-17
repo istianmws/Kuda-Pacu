8737434d71897d7d569753a6180329f2625f5486
alfajri23 <123190088@student.upnyk.ac.id>
2024-08-17 16:22:04 +0700

    Halaman awal

--- src/interfaces/card.ts
+++ src/interfaces/card.ts
@@ -1,7 +1,12 @@
+import type Pengiklan from "./pengiklans";
+
 export default interface Card {
     id: number;
     attributes: {
       judul: string;
       deskripsi: string;
+      pengiklans: {
+        data: Pengiklan;
+      };
     };
 }
---
+++ src/interfaces/pengiklans.ts
@@ -0,0 +1,12 @@
+export default interface Pengiklan {
+    id: number;
+    attributes: {
+      nama: string;
+      whatsapp: string;
+      instagram: string;
+      youtube: string;
+      tiktok: string;
+      facebook: string;
+      website: string;
+    };
+}
