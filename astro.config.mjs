4718917e5c48d66ad00acaf94c1924bded53bfd5
istianmws <istianmuhammad@gmail.com>
2024-08-19 19:28:36 +0700

    refactor: adapter ssr vercel punya sendiri

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,22 @@
 import { defineConfig } from 'astro/config';
-
-
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   integrations: [tailwind()]
 });
 
 // export default defineConfig({
-  
+
 //   // output: 'server',
 //   // adapter: node({
 //   //   mode: 'standalone',
 //   // }),
-  
+
 //   integrations: [tailwind()]
 // });
