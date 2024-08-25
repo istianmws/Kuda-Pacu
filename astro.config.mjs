20c96046ca6a095eccb68c0d12707f22006d1ea9
alfajri23 <123190088@student.upnyk.ac.id>
2024-08-25 20:27:29 +0700

    Fixing astro md ke vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+//import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,10 +13,10 @@ export default defineConfig({
 
 // export default defineConfig({
 
-//   // output: 'server',
-//   // adapter: node({
-//   //   mode: 'standalone',
-//   // }),
+//   output: 'server',
+//   adapter: node({
+//     mode: 'standalone',
+//   }),
 
 //   integrations: [tailwind()]
 // });
