1ee051f494f72b028c38156b4dd275193b13d427
istianmws <istianmuhammad@gmail.com>
2024-08-26 21:10:54 +0700

    refactor: Remove commented code and optimize Astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,18 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  integrations: [tailwind()]
-});
-
 // export default defineConfig({
-
 //   output: 'server',
-//   adapter: node({
-//     mode: 'standalone',
-//   }),
-
+//   adapter: vercel(),
 //   integrations: [tailwind()]
 // });
+
+export default defineConfig({
+
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+  integrations: [tailwind()]
+});
